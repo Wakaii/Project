0f8c7daa2247c134efad0b2ca2bdada52c6c35f1
Kai Wang <v-wakai@microsoft.com>
2020-04-17 02:31:05 -0700

    Init project

---
+++ LoginDemo/LoginDemo/Data/LoginDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LoginDemo.Models;
+
+namespace LoginDemo.Data
+{
+    public class LoginDemoContext : DbContext
+    {
+        public LoginDemoContext (DbContextOptions<LoginDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoginDemo.Models.Movie> Movie { get; set; }
+    }
+}
